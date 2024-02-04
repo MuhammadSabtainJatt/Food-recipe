2e4e9461016fa792f64a65ec6889247354167930
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-02-04 22:12:33 +0500

    Whole setup is created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import './App.scss';
+
+function App() {
+  return (
+    <>
+    <h1>
+      HOME
+    </h1>
+    </>
+  );
+}
+
+export default App;
