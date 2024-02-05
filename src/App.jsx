224820bebd06541e0994d6ccdd22ea6fc7b563e3
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-02-05 21:01:16 +0500

    Tweaks in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
+import React, { useEffect, useState } from 'react';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import './App.scss';
 
-function App() {
+function MealSearch() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [meals, setMeals] = useState([]);
+  const [error, setError] = useState(null);
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+
+    handleSearch(searchTerm)
+  };
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch meals');
+      }
+      const data = await response.json();
+      setMeals(data.meals || []);
+      console.log(meals)
+      setError(null);
+    } catch (error) {
+      setError(error.message);
+
+    }
+  };
+
   return (
     <>
-    <h1>
-      HOME
-    </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">Button</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
 
-export default App;
+export default MealSearch;
