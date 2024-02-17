8191774c5cf8dc666aa9029a62b0e173e7ebca0b
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-02-17 21:00:10 +0500

    Tweaks in code

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -24,4 +24,108 @@ export default function Loader() {
         </>
     );
 }               
+import React from 'react';
+
+export default function Loader() {
+    const cells = 4;
+
+    const renderCells = () => {
+        const cellElements = [];
+        for (let i = 0; i < cells; i++) {
+            for (let j = 0; j < cells; j++) {
+                const className = `cell d-${i + j}`;
+                cellElements.push(<div className={className} key={`${i}-${j}`} />);
+            }
+        }
+        return cellElements;
+    };
+
+    return (
+        <>
+            <div className="body">
+                <div className="mosaic-loader">
+                    {renderCells()}
+                </div>
+            </div>
+        </>
+    );
+}               
+import React from 'react';
+
+export default function Loader() {
+    const cells = 4;
+
+    const renderCells = () => {
+        const cellElements = [];
+        for (let i = 0; i < cells; i++) {
+            for (let j = 0; j < cells; j++) {
+                const className = `cell d-${i + j}`;
+                cellElements.push(<div className={className} key={`${i}-${j}`} />);
+            }
+        }
+        return cellElements;
+    };
+
+    return (
+        <>
+            <div className="body">
+                <div className="mosaic-loader">
+                    {renderCells()}
+                </div>
+            </div>
+        </>
+    );
+}               
+import React from 'react';
+
+export default function Loader() {
+    const cells = 4;
+
+    const renderCells = () => {
+        const cellElements = [];
+        for (let i = 0; i < cells; i++) {
+            for (let j = 0; j < cells; j++) {
+                const className = `cell d-${i + j}`;
+                cellElements.push(<div className={className} key={`${i}-${j}`} />);
+            }
+        }
+        return cellElements;
+    };
+
+    return (
+        <>
+            <div className="body">
+                <div className="mosaic-loader">
+                    {renderCells()}
+                </div>
+            </div>
+        </>
+    );
+}               
+import React from 'react';
+
+export default function Loader() {
+    const cells = 4;
+
+    const renderCells = () => {
+        const cellElements = [];
+        for (let i = 0; i < cells; i++) {
+            for (let j = 0; j < cells; j++) {
+                const className = `cell d-${i + j}`;
+                cellElements.push(<div className={className} key={`${i}-${j}`} />);
+            }
+        }
+        return cellElements;
+    };
+
+    return (
+        <>
+            <div className="body">
+                <div className="mosaic-loader">
+                    {renderCells()}
+                </div>
+            </div>
+        </>
+    );
+}               
         
